afa9f3a9c5f9dfee1337a9722676710b7c92ede1
xrlcoach <xrlcoach@gmail.com>
2020-12-15 19:08:20 +1100

    Changed bad getElementById

--- js/squads.js
+++ js/squads.js
@@ -16,16 +16,16 @@ fetch('https://cyy6ekckwa.execute-api.ap-southeast-2.amazonaws.com/Test1/players
     for (var i = 0; i < data.length; i++) {
         var player = data[i];
         var tr = document.createElement('tr');
-        name = document.createElement('td');
+        var name = document.createElement('td');
         name.textContent = player.player_name;
         tr.appendChild(name);
-        pos1 = document.createElement('td');
+        var pos1 = document.createElement('td');
         pos1.textContent = player.position;
         tr.appendChild(pos1);
-        pos2 = document.createElement('td');
+        var pos2 = document.createElement('td');
         pos2.textContent = player.position2;
         tr.appendChild(pos2);
-        team = document.createElement('td');
+        var team = document.createElement('td');
         team.textContent = player.nrl_team;
         tr.appendChild(team);
         tableBody.appendChild(tr);

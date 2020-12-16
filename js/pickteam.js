0a4a2004e01ecc0f4af72cbef35e7d98486871de
xrlcoach <xrlcoach@gmail.com>
2020-12-17 09:37:57 +1100

    Testing pick player func

--- js/pickteam.js
+++ js/pickteam.js
@@ -13,18 +13,34 @@ GetActiveUserInfo(idToken)
                     .then((data) => {
                         PopulatePickPlayerTable(data, user.team_short, 'pickPlayerTable');
                     });
-                });
+            });
     })
-    .catch((error) => document.getElementById('feedback').innerHTML = error);  
+    .catch((error) => document.getElementById('feedback').innerHTML = error);
 
-
-function selectNrlClub(event) {
-    event.preventDefault();
-    club = document.getElementById('nrlClubSelect').value;
-    document.getElementById('squadName').innerText = club;
-    GetPlayersFromNrlClub(club)
+function DisplayPlayerCounts(xrlTeam) {
+    GetPlayersFromXrlTeam(xrlTeam)
         .then((data) => {
-            PopulatePickPlayerTable(data, user.team_short, 'pickPlayerTable');
+            var totalPlayers = data.length;
+            if (totalPlayers == 18) {
+                window.location.href = 'index.html';
+            }
+            var backs = data.filter(p => p.position == 'Back' || p.position2 == 'Back').length
+            var forwards = data.filter(p => p.position == 'Forward' || p.position2 == 'Forward').length
+            var playmakers = data.filter(p => p.position == 'Playmaker' || p.position2 == 'Playmaker').length
+            document.getElementById('playerCountMessage').innerText =
+                `You currently have ${totalPlayers} in your squad. You need ${18 - totalPlayers} more in total.`
+            if (backs.length < 5) {
+                document.getElementById('playerCountBreakdown').innerHTML +=
+                    `<li>You need at least ${5 - backs.length} more backs.`
+            }
+            if (forwards.length < 5) {
+                document.getElementById('playerCountBreakdown').innerHTML +=
+                    `<li>You need at least ${5 - forwards.length} more forwards.`
+            }
+            if (playmakers.length < 3) {
+                document.getElementById('playerCountBreakdown').innerHTML +=
+                    `<li>You need at least ${3 - playmakers.length} more playmakers.`
+            }
         })
         .catch((error) => {
             document.getElementById('feedback').innerText = error;
@@ -71,11 +87,11 @@ function PopulatePickPlayerTable(playerData, xrlTeam, tableId) {
                 form.onsubmit = (event) => {
                     event.preventDefault();
                     UpdatePlayerXrlTeam(null, input.value)
-                        .then(() => DisplayPlayerCounts(xrlTeam))                    
+                        .then(() => DisplayPlayerCounts(xrlTeam))
                 };
             }
             form.appendChild(button);
-            pickOrDrop.appendChild(form);            
+            pickOrDrop.appendChild(form);
             tr.appendChild(pickOrDrop);
         } else {
             var xrl = document.createElement('td');
@@ -86,30 +102,13 @@ function PopulatePickPlayerTable(playerData, xrlTeam, tableId) {
     }
 }
 
-function DisplayPlayerCounts(xrlTeam) {
-    GetPlayersFromXrlTeam(xrlTeam)
+function selectNrlClub(event) {
+    event.preventDefault();
+    club = document.getElementById('nrlClubSelect').value;
+    document.getElementById('squadName').innerText = club;
+    GetPlayersFromNrlClub(club)
         .then((data) => {
-            var totalPlayers = data.length;
-            if (totalPlayers == 18) {
-                window.location.href = 'index.html';
-            }
-            var backs = data.filter(p => p.position == 'Back' || p.position2 == 'Back').length
-            var forwards = data.filter(p => p.position == 'Forward' || p.position2 == 'Forward').length
-            var playmakers = data.filter(p => p.position == 'Playmaker' || p.position2 == 'Playmaker').length
-            document.getElementById('playerCountMessage').innerText =
-                `You currently have ${totalPlayers} in your squad. You need ${18 - totalPlayers} more in total.`
-            if (backs.length < 5) {
-                document.getElementById('playerCountBreakdown').innerHTML +=
-                    `<li>You need at least ${5 - backs.length} more backs.`
-            }
-            if (forwards.length < 5) {
-                document.getElementById('playerCountBreakdown').innerHTML +=
-                    `<li>You need at least ${5 - forwards.length} more forwards.`
-            }
-            if (playmakers.length < 3) {
-                document.getElementById('playerCountBreakdown').innerHTML +=
-                    `<li>You need at least ${3 - playmakers.length} more playmakers.`
-            }
+            PopulatePickPlayerTable(data, user.team_short, 'pickPlayerTable');
         })
         .catch((error) => {
             document.getElementById('feedback').innerText = error;

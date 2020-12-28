fe7e22c8b2c81b5c612292bb3e1bc881834ded38
xrlcoach <xrlcoach@gmail.com>
2020-12-29 10:02:32 +1100

    Debugging changes

--- js/login.js
+++ js/login.js
@@ -5,11 +5,11 @@ async function login(event) {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;    
     let idToken = await Login(username, password);
-    var now = new Date();
-    var expiryTime = now.setHours(now.getHours() + 1);
+    var expiry = new Date();
+    expiry.setHours(expiry.getHours() + 1);
     document.cookie = `id=${idToken}; expiry=${expiryTime.toUTCString()}; Secure`;
     let roundInfo = await GetCurrentRoundInfo();
-    document.cookie = `round=${roundInfo.round_number}; expiry=${expiryTime.toUTCString()}; Secure`;
+    document.cookie = `round=${roundInfo.round_number}; expires=${expiry.toUTCString()}; Secure`;
     window.location.href = './index.html';
 }
 

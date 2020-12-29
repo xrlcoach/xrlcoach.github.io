00c1e5d24b37c2346aeb86845ae19492c841fa58
xrlcoach <xrlcoach@gmail.com>
2020-12-29 20:34:09 +1100

    Debuggin stats page

--- js/stats.js
+++ js/stats.js
@@ -6,14 +6,16 @@ let roundToDisplay, allStats, allUsers, activeUser, allPlayers, playersTotalStat
 window.onload = async function() {
     roundToDisplay = await GetCurrentRoundInfo();
     allStats = await GetAllStats();
+    playerIdsWithStats = allStats.map(p => p.player_id);
     allUsers = await GetAllUsers();
     activeUser = await GetActiveUserInfo(GetIdToken());
     allPlayers = await GetAllPlayers();
-    playersTotalStats = allPlayers.map(function(p) {
+    allPlayersWithStats = allPlayers.filter(p => playerIdsWithStats.includes(p.player_id))
+    playersTotalStats = allPlayersWithStats.map(function(p) {
         let playerStats = allStats.filter(s => s.player_id == p.player_id);
-        if (playerStats.length == 0) {
-            return;
-        }
+        // if (playerStats.length == 0) {
+        //     return;
+        // }
         let playerStatsWithScores = playerStats.map(a => {
             a.score = GetPlayerXrlScores(p.position, a);
             return a;

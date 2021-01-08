67b296cb7a2df500cd8dee0d2d5f46e3bc8d6261
xrlcoach <xrlcoach@gmail.com>
2021-01-08 11:25:53 +1100

    Adding feedback to login/signup

--- js/signup.js
+++ js/signup.js
@@ -3,11 +3,47 @@ import { DisplayFeedback } from "./Helpers.js";
 function signUp(event) {
     event.preventDefault();
     var username = document.getElementById('username').value;
+    if (username == '' || username.length < 3) {
+        DisplayFeedback('Error', 'Please enter a valid username');
+        return;
+    }
     var password = document.getElementById('password').value;
+    if (password == '' || password.length < 8) {
+        DisplayFeedback('Error', 'Please enter a valid password (at least 8 characters, with one uppercase, one number, one special character. You know the drill.)');
+        return;
+    }
+    var passwordConfirm = document.getElementById('confirm_password').value;
     var teamName = document.getElementById('team_name').value;
-    var teamShort = document.getElementById('team_short').value;
+    if (teamName == '') {
+        DisplayFeedback('Error', 'Please enter a valid team name');
+        return;
+    }
+    var teamShort = document.getElementById('team_short').value.toUpperCase();
+    if (teamShort == '') {
+        DisplayFeedback('Error', 'Please enter a valid team acronym');
+        return;
+    }
     var homeground = document.getElementById('homeground').value;
-
+    if (homeground == '') {
+        DisplayFeedback('Error', 'Please enter a valid ground name');
+        return;
+    }
+    if (password != passwordConfirm) {
+        DisplayFeedback('Error', 'Passwords do not match');
+        return;
+    }
+    if (teamName.length > 150) {
+        DisplayFeedback('Error', 'Team name too long.');
+        return;
+    }
+    if (teamShort.length != 3) {
+        DisplayFeedback('Error', 'Team acronym must be three letters');
+        return;
+    }
+    if (homeground.length > 150) {
+        DisplayFeedback('Error', 'Homeground name too long.');
+        return;
+    }
 
     fetch('https://cyy6ekckwa.execute-api.ap-southeast-2.amazonaws.com/Test1/xrl-users/signup', {
     method: 'POST',

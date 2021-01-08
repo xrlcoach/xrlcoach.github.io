e8867ffb7008717bedc4be24d179865fa6687549
xrlcoach <xrlcoach@gmail.com>
2021-01-09 09:47:21 +1100

    Fixing first login experience

--- js/fixture.js
+++ js/fixture.js
@@ -1,7 +1,7 @@
 /* Script controlling fixture.html, which displays XRL match stats */
 
 import { GetLineupByTeamAndRound, GetRoundInfo, getCookie, GetRoundInfoFromCookie, GetActiveUserInfo, GetIdToken, GetActiveUserTeamShort } from "./ApiFetch.js";
-import { DisplayAppearanceInfoFromLineup, GetLineupScore, GetTeamFixture, PositionNames } from "./Helpers.js";
+import { DisplayAppearanceInfoFromLineup, GetLineupScore, GetTeamFixture, PositionNames, DisplayFeedback } from "./Helpers.js";
 
 let roundNumber, roundInfo, completed, match, homeTeam, awayTeam, homeLineup, awayLineup;
 

b0949fa6cb890a2ebad8228813cb3f4e7f98204d
xrlcoach <xrlcoach@gmail.com>
2021-01-13 17:04:27 +1100

    Debugging waiver system

--- js/Session.js
+++ js/Session.js
@@ -1,5 +1,6 @@
 function logout() {
     document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "round=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "team=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.replace('login.html');
 }
